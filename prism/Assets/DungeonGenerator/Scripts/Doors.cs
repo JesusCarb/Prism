5ab83901324dd8edb37b5de508ae2e05dbdac54e
Origin <tylerslakman@gmail.com>
2023-11-16 17:27:11 -0500

    Update Doors.cs

--- prism/Assets/DungeonGenerator/Scripts/Doors.cs
+++ prism/Assets/DungeonGenerator/Scripts/Doors.cs
@@ -4,22 +4,6 @@ using UnityEngine;
 
 public class Doors : MonoBehaviour
 {
-    public enum doorType
-    {
-        up = 0,
-        down = 1,
-        left = 2,
-        right = 3
-    }
-    public int doorTypeNum;
-
-    public struct Door
-    {
-        public bool active;
-        public bool locked;
-        public Door leadsTo;
-        public WalkableRoom leadsToRoom; 
-    }
     // Start is called before the first frame update
     void Start()
     {
