8a8a23e337b5dc2dbb7d1406068acc770a6fa2b6
RemyMB <brandonbedard12@gmail.com>
2023-11-29 04:00:37 -0500

    shsfdgs
    sdfgsdgfs

--- prism/Assets/DungeonGenerator/Scripts/TransitionController.cs
+++ prism/Assets/DungeonGenerator/Scripts/TransitionController.cs
@@ -8,6 +8,8 @@ public class TransitionController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //new WaitForSeconds(2);
+        //SceneManager.LoadScene("DungeonGen");
         StartCoroutine(SendToDungeon());
     }
 
